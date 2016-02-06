fa6fe5a6177a030cddd331ee72787feca3a19a91
Ma <machrivi99@gmail.com>
2016-02-06 17:58:56 +0100

    max

--- src/ngb/Manager.java
+++ src/ngb/Manager.java
@@ -5,8 +5,8 @@ import util.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 /**
  * Created by Florian on 06.02.2016.
@@ -42,32 +42,11 @@ public class Manager implements DrawInferface, FrameInitInterface, Tickable {
 
     @Override
     public void initFrame(JFrame f, DrawFrame.DrawPanel dp) {
-        dp.addMouseListener(new MouseListener() {
+        dp.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-
+                super.mouseClicked(e);
             }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-
-            }
-
         });
     }
 
