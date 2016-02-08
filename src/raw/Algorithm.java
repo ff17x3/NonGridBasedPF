919b79c0d9f3f7caa7bd97061958afc66636e70b
ff17x3 <ff17x3@gmail.com>
2016-02-08 19:20:01 +0100

    flo

--- src/raw/Algorithm.java
+++ src/raw/Algorithm.java
@@ -1,7 +1,15 @@
 package raw;
 
+import simulation.Obstacle;
+
 /**
- * Created by Ma on 08.02.2016.
+ * Created by Florian on 08.02.2016.
  */
 public class Algorithm {
+
+
+    public static MatrixPosBundle genMap(Obstacle[] obstacles) {
+        return null;
+    }
+
 }
