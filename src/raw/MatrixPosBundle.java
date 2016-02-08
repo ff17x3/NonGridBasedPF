801b8649bb629ec49e0e2fed01515478ff314519
ff17x3 <ff17x3@gmail.com>
2016-02-08 19:24:54 +0100

    flo

--- src/raw/MatrixPosBundle.java
+++ src/raw/MatrixPosBundle.java
@@ -1,7 +1,21 @@
 package raw;
 
+import util.PointF;
+
 /**
  * Created by Florian on 08.02.2016.
  */
 public class MatrixPosBundle {
+
+    public final float[][] matrix;
+    public final PointF[] nodesPos;
+    public final byte[] expandDir;
+
+    public MatrixPosBundle(float[][] matrix, PointF[] nodesPos, byte[] expandDir) {
+        this.matrix = matrix;
+        this.nodesPos = nodesPos;
+        this.expandDir = expandDir;
+    }
+
+
 }
